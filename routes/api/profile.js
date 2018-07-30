7209c47b479784fb606636e2a1307004769dc0f7
elshack09 <elshack09@gmail.com>
2018-07-30 16:59:29 -0400

    added deleted file back

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require('mongoose')
+
+const Profile = require('../../models/Profile')
+
+const User = require('../../models/User')
+
 
 // GET route
 router.get("/", (req, res) => res.json({ msg: "Route for Profile" }));
+
+
+// Profile for current user
 
 module.exports = router;
