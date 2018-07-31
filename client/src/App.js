a6dac1ec3a8cb9f3cd0a04dac0c0d774d2fcd191
elshack09 <elshack09@gmail.com>
2018-07-31 11:28:27 -0400

    login modal added

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router,  Route } from "react-router-dom";
 import Navbartop from './components/Navbartop';
 import Imagehomepage from './components/Imagehomepage';
 import FooterPage from './components/Footerpage';
+import Profileform from './components/Profileform';
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
-        <Navbartop /> <br/>
-        <div>
-          <Imagehomepage/>
+        <Navbartop /> <br/> 
+        
+       <Route exact path="/" component={Imagehomepage}/>
+       <div className="container">
+        <Route exact path="/profile/form" component={Profileform}/>
         </div> 
          <FooterPage />
       </div>
-    
+    </Router>
     );
   }
 }
--- client/src/components/Navbartop.js
+++ client/src/components/Navbartop.js
@@ -1,50 +1,128 @@
-import React, { Component } from 'react';
-import { Navbar, NavbarBrand, NavbarNav, NavbarToggler, Collapse, NavItem, NavLink, Button} from 'mdbreact';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React, { Component } from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarNav,
+  NavbarToggler,
+  Collapse,
+  NavItem,
+  NavLink,
+  Row,
+  Col,
+  Input,
+  Button,
+  Fa,
+  Modal,
+  ModalBody,
+  ModalFooter,
+  Container
+} from "mdbreact";
+import { BrowserRouter as Router } from "react-router-dom";
 
 class Navbartop extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            collapse: false,
-           
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapse: false,
+      modal: false
+    };
     this.onClick = this.onClick.bind(this);
     this.toggle = this.toggle.bind(this);
-    }
+  }
 
-    onClick(){
-        this.setState({
-            collapse: !this.state.collapse,
-        });
-    }
+  onClick() {
+    this.setState({
+      collapse: !this.state.collapse
+    });
+  }
 
-    toggle() {
-        this.setState({
-            dropdownOpen: !this.state.dropdownOpen
-        });
-    }
+  toggle() {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen,
+      modal: !this.state.modal
+    });
+  }
 
-    render() {
-        return (
-            <Router>
-                <Navbar color="grey darken-4" dark expand="md" scrolling>
-                    <NavbarBrand href="/">
-                        <h1 class="h1-responsive white-text">LIFT</h1>
-                    </NavbarBrand>
-                    { !this.state.isWideEnough && <NavbarToggler onClick = { this.onClick } />}
-                    <Collapse isOpen = { this.state.collapse } navbar>
-                       
-                        <NavbarNav right>
-                        <Button type="button" class="btn red darken-4 btn-rounded waves-effect">Login</Button>
-                        <Button type="button" class="btn red darken-4 btn-rounded waves-effect">SignUp</Button>
-                        </NavbarNav>
-                    </Collapse>
-                </Navbar>
-                
-            </Router>
-        );
-    }
+  render() {
+    return (
+      <Router>
+        <Navbar color="grey darken-4" dark expand="md" scrolling>
+          <NavbarBrand href="/">
+            <h1 class="h1-responsive white-text">LIFT</h1>
+          </NavbarBrand>
+          {!this.state.isWideEnough && <NavbarToggler onClick={this.onClick} />}
+          <Collapse isOpen={this.state.collapse} navbar>
+            <NavbarNav right>
+              <Container>
+                <Row>
+                  <Col md="6">
+                    <Button
+                      color="btn red darken-4 btn-rounded waves-effect"
+                      onClick={this.toggle}
+                    >
+                      Login
+                      <Modal
+                        isOpen={this.state.modal}
+                        toggle={this.toggle}
+                        className="cascading-modal"
+                      >
+                        <div className="modal-header red darken-4 white-text">
+                          <h4 className="title">
+                            <Fa className="fa fa-sign-in" />Login
+                          </h4>
+                          <button
+                            type="button"
+                            className="close"
+                            onClick={this.toggle}
+                          >
+                            <span aria-hidden="true">×</span>
+                          </button>
+                        </div>
+                        <ModalBody className="grey-text">
+                          <Input
+                            size="sm"
+                            label="Your email"
+                            icon="envelope"
+                            group
+                            type="email"
+                            validate
+                            error="wrong"
+                            success="right"
+                          />
+                          <Input
+                            size="sm"
+                            label="password"
+                            icon="lock"
+                            group
+                            type="password"
+                            validate
+                            error="wrong"
+                            success="right"
+                          />
+                        </ModalBody>
+                        <ModalFooter>
+                          <Button color="red darken-4" onClick={this.toggle}>
+                            Close
+                          </Button>{" "}
+                          <Button color="red darken-4">Login</Button>
+                        </ModalFooter>
+                      </Modal>
+                    </Button>
+                  </Col>
+                </Row>
+              </Container>
+            </NavbarNav>
+          </Collapse>
+          <Button
+            type="button"
+            class="btn red darken-4 btn-rounded waves-effect"
+          >
+            SignUp
+          </Button>
+        </Navbar>
+      </Router>
+    );
+  }
 }
 
-export default Navbartop
+export default Navbartop;
