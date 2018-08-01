bef86d84d8966660bf309a192b856f935eb0a30f
elshack09 <elshack09@gmail.com>
2018-08-01 17:33:05 -0400

    updates script

--- server.js
+++ server.js
@@ -1,55 +1,42 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const path = require('path')
-
-
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const path = require("path");
 
 // routes for api
-const users = require('./routes/api/users')
-const profile = require('./routes/api/profile')
-const exercises = require('./routes/api/exercises')
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const exercises = require("./routes/api/exercises");
 
 const app = express();
 // middleware
-app.use(bodyParser.urlencoded({extended:false}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-
-
-
-// mLab info for db 
-const database = require('./config/keys').mongoURI
+// mLab info for db
+const database = require("./config/keys").mongoURI;
 
 // Connecting to MongoDB
 mongoose
-.connect(database)
-.then(() => console.log('MongoDB up and running!'))
-.catch(err => console.log(err))
+  .connect(database)
+  .then(() => console.log("MongoDB up and running!"))
+  .catch(err => console.log(err));
 
-
-app.get('/', (req, res) => res.send('Working'))
-
-
+app.get("/", (req, res) => res.send("Working"));
 
 // App Routes
-app.use('/api/users', users)
-app.use('/api/profile', profile)   
-app.use('/api/exercises', exercises)
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/exercises", exercises);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 
-// build 
-if (process.env.NODE_ENV === 'production') {
- 
-  app.use('/static', express.static(path.join(__dirname, 'client/build')))
-  
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
-  
-
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 3001;
 
-app.listen(port, () => console.log(`Server running ${port}`))
+app.listen(port, () => console.log(`Server running ${port}`));
